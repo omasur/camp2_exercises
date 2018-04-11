35351dc0b06bc07095d4020b29e45c18a6dbe871
Vincent Billey <vincent.billey@gmail.com>
2018-04-11 13:43:20 +0200

    do not expect a return to allow for async usage

--- 02_programming_fundamentals/07_file_management/01_basic_fs/.__tests__/delete_file.test.js
+++ 02_programming_fundamentals/07_file_management/01_basic_fs/.__tests__/delete_file.test.js
@@ -3,7 +3,8 @@ const path = require("path");
 
 const deleteFile = require("../02_delete_file");
 
-const fileToRemove = () => fs.realpathSync("/tmp") + "/deleteFile/file_to_rm.txt";
+const fileToRemove = () =>
+  fs.realpathSync("/tmp") + "/deleteFile/file_to_rm.txt";
 
 const createTreeStructure = () => {
   if (!fs.existsSync(path.dirname(fileToRemove()))) {
@@ -13,7 +14,7 @@ const createTreeStructure = () => {
   if (!fs.existsSync(fileToRemove())) {
     fs.writeFileSync(fileToRemove(), "Hello World");
   }
-}
+};
 
 const removeTreeStructure = () => {
   if (fs.existsSync(fileToRemove())) {
@@ -23,21 +24,37 @@ const removeTreeStructure = () => {
   if (fs.existsSync(path.basename(fileToRemove()))) {
     fs.rmdirSync(path.basename(fileToRemove()));
   }
-}
+};
 
 afterEach(removeTreeStructure);
 beforeEach(createTreeStructure);
 
-test(`should remove an existing file`, () => {
-  expect(deleteFile(fileToRemove())).toEqual(true);
-  expect(fs.existsSync(fileToRemove())).toEqual(false)
+test(`should remove an existing file`, done => {
+  expect.assertions(1);
+  deleteFile(fileToRemove());
+  setTimeout(function() {
+    expect(fs.existsSync(fileToRemove())).toEqual(false);
+    done();
+  }, 500);
 });
 
-test(`should not remove a non existing file`, () => {
-  expect(deleteFile(path.dirname(fileToRemove()) + '/non-existing-file')).toEqual(false);
+test(`should not remove a non existing file`, done => {
+  expect.assertions(1);
+  console.warn = jest.fn();
+  deleteFile(path.dirname(fileToRemove()) + "/non-existing-file");
+  setTimeout(function() {
+    expect(console.warn).toHaveBeenCalled();
+    done();
+  }, 500);
 });
 
-test(`should not remove a folder`, () => {
-  expect(deleteFile(path.dirname(fileToRemove()))).toEqual(false);
-  expect(fs.existsSync(path.dirname(fileToRemove()))).toEqual(true)
+test(`should not remove a folder`, done => {
+  expect.assertions(2);
+  console.warn = jest.fn();
+  deleteFile(path.dirname(fileToRemove()));
+  setTimeout(function() {
+    expect(fs.existsSync(path.dirname(fileToRemove()))).toEqual(true);
+    expect(console.warn).toHaveBeenCalled();
+    done();
+  }, 500);
 });
