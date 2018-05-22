879e4e80b5225ac30c98d9b0632cfafea3d4d32a
OLIVIER MASUREL <olivier.masurel@decathlon.COM>
2018-05-22 08:05:21 +0200

    React fund restruct

--- 06_react_redux/01_es6_fundamentals/01_aquarium/01_aquarium.js
+++ 06_react_redux/01_es6_fundamentals/01_aquarium/01_aquarium.js
@@ -1,5 +1,53 @@
 // Insert code here
+class Aquarium {
+  constructor(fishes = [], algaes =[] /* Other arguments */) {
+    this.fishes = fishes;
+    this.algaes = algaes;
+  }
+  addFish(fish) {
+    this.fishes.push(fish);
+  }
+  addAlgae(algae) {
+    this.algaes.push(algae);
+  }
+  endTurn() {
+    console.log("Algae length: ",this.algaes.length);
+    this.fishes.forEach((fish) => {
+      console.log("Fish name:", fish.name, " - sex:", fish.sex);
+    })
 
+    this.fishes.forEach ((element) => {
+      if (!(element.isCarnivor) && (this.algaes.length > 0)) {
+        console.log("C est un vegan et il peut prendre une algue");
+        this.algaes.pop();
+      } else if ((element.isCarnivor) && (this.fishes.length > 1)) {
+        console.log("C est un carn et il peut manger");
+        this.fishes.pop();
+      }
+
+    });
+  }
+}
+
+class Fish {
+  constructor(name, sex, isCarnivor =true/* Other arguments */) {
+    this.name = name;
+    this.sex = sex;
+    this.isCarnivor = isCarnivor;
+  }
+}
+
+class Algae {
+  constructor(size /* Other arguments */) {
+    this.size = size;
+  }
+}
+
+let aqua = new Aquarium([new Fish('Toto', 'F', 0)],[new Algae(5)]);
+aqua.addFish(new Fish('titi', 'M', 1))
+aqua.addFish(new Fish('tata', 'F', 0))
+console.log(aqua);
+aqua.endTurn();
 
 
 module.exports = {
--- 06_react_redux/01_es6_fundamentals/02_spread_destructuring/02_spread_destructuring.js
+++ 06_react_redux/01_es6_fundamentals/02_spread_destructuring/02_spread_destructuring.js
@@ -1,7 +1,7 @@
 function go(options) {
   let {
     speed = 4,
-    enable: { hyperdrive = false, frobnifier = true}
+    enable: { hyperdrive = false, frobnifier = true} = {}
   } = options;
 
   console.log("speed=", speed, "hyperdrive:", hyperdrive, "frobnifier:", frobnifier);
@@ -9,7 +9,7 @@ function go(options) {
 
 go({ speed: 5 });
 
-function lastIndexOf(arr, elt, start) {
+function lastIndexOf(arr, elt, start=arr.length) {
   for (let i = start - 1; i >= 0; i--) {
     if (arr[i] === elt) {
       return i;
@@ -22,7 +22,8 @@ function lastIndexOf(arr, elt, start) {
 console.log(lastIndexOf([1, 2, 1, 2], 2));
 
 function replace(array, from, to, elements) {
-  array.splice.apply(array, [from, to - from].concat(elements));
+  // array.splice.apply(array, [from, to - from].concat(elements));
+  array.splice(from, to - from, ...elements);
 }
 
 let testArray = [1, 2, 100, 100, 6];
@@ -30,7 +31,8 @@ replace(testArray, 2, 4, [3, 4, 5]);
 console.log(testArray);
 
 function copyReplace(array, from, to, elements) {
-  return array.slice(0, from).concat(elements).concat(array.slice(to));
+  // return array.slice(0, from).concat(elements).concat(array.slice(to));
+  return [...array.slice(0, from), ...elements, ...(array.slice(to))];
 }
 
 console.log(copyReplace([1, 2, 100, 200, 6], 2, 4, [3, 4, 5]));
